8adaa45f60b3c298437245d4c65086f86fcf1cd6
Josh Zeller <jdzeller18@gmail.com>
2020-01-22 14:28:08 -0600

    Update lvm-gen-prompt.py

--- lvm-gen-prompt.py
+++ lvm-gen-prompt.py
@@ -5,7 +5,9 @@ vg = input('Volume Group: ')
 lv = input('Logical Volume(s): ')
 gb = input('Size in GBs: ')
 
+# Determine of single entry or comma separated list
 if pv.find(',') != -1:
+    # If comma separated list split string into list
     pv = pv.split(',')
     for i in range(len(pv)):
         print('pvcreate /dev/' + pv[i])
@@ -23,7 +25,7 @@ if lv.find(',') != -1:
 
     for i in range(len(lv)):
         if i < (len(lv) - 1):
-            print('lvcreate -L ' + str(gb[i]) + ' -n ' + lv[i] + ' ' + vg)
+            print('lvcreate -L ' + str(gb[i]) + 'G -n ' + lv[i] + ' ' + vg)
         else:
             print('lvcreate -l +100%FREE -n ' + lv[i] + ' ' + vg)
 else:
